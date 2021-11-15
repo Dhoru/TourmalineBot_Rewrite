00b1834347d5fcc117b5564cfe32b5b2c21552c4
Dhoru <52000ammar@gmail.com>
2021-11-15 17:49:10 +0500

    gogwin

--- cogs/ownerCommands.py
+++ cogs/ownerCommands.py
@@ -10,19 +10,22 @@ class ownerCommands(commands.Cog):
 	@commands.command(name='unload')
 	@commands.is_owner()
 	async def unload(self, ctx: commands.Context, args):
-		ctx.bot.unload_extension(args)
+		cogLoaderText = 'cogs.' + args
+		ctx.bot.unload_extension(cogLoaderText)
 		await ctx.message.add_reaction("<:greentick:719778001373364304>")
 
 	@commands.command(name='load')
 	@commands.is_owner()
 	async def load(self, ctx: commands.Context, args):
-		ctx.bot.load_extension(args)
+		cogLoaderText = 'cogs.' + args
+		ctx.bot.load_extension(cogLoaderText)
 		await ctx.message.add_reaction("<:greentick:719778001373364304>")
 
 	@commands.command(name='reload')
 	@commands.is_owner()
 	async def reload(self, ctx: commands.Context, args):
-		ctx.bot.reload_extension(args)
+		cogLoaderText = 'cogs.' + args
+		ctx.bot.reload_extension(cogLoaderText)
 		await ctx.message.add_reaction("<:greentick:719778001373364304>")
 
 	@commands.command(name="setstatus", help="Changes the status of the bot", Hidden=True)
