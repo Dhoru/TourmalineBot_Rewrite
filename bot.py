d8c849a1b56e16899463392d371a00704ad9fe9a
Dhoru <52000ammar@gmail.com>
2022-03-06 18:07:14 +0500

    ups

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ bot = commands.Bot(command_prefix="t.", description=description, intents=discord
 bot.remove_command('help')
 
 bot.load_extension("cogs.mainCommands")
-bot.load_extension("cogs.listener")
 bot.load_extension("cogs.ownerCommands")
 bot.load_extension("cogs.helpMe")
 bot.load_extension("cogs.miscCommands")
